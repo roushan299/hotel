78e421e4405e16055557c61b33fb370cf3750279
Roushan kumar <roushankumar7231246@gmail.com>
2023-12-25 03:06:11 +0530

    form based authentication is added

---
+++ src/main/java/com/booking/hotel/config/HotelSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.booking.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class HotelSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails user1 = User.builder()
+                .username("tony")
+                .password(passwordEncoder().encode("password"))
+                .roles("NORMAL")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("steve")
+                .password(passwordEncoder().encode("nopassword"))
+                .roles("NORMAL")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
